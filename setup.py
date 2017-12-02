0c21c38782d5d9a33e5c36c550c78202cef9ea0c
Igor <igor955@gmail.com>
2017-12-02 23:36:24 +0100

    setup.py fix

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         'Environment :: Console',
         'Environment :: Web Environment'
     ],
-    install_requires=['Flask', 'click>=6', 'requests', 'sphinx-click', 'sphinx_rtd_theme'],
+    install_requires=['Flask', 'click>=6', 'requests', 'sphinx_click', 'sphinx_rtd_theme'],
     zip_safe=False,
     entry_points={
         'console_scripts': [
